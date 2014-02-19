559ce5cff8033c9d579940a164a7e61486f4d09c
Jay Moon <jaymoon@stanford.edu>
2014-02-18 20:18:22 -0800

    lab completed

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,9 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project
+    .find({"_id": projectID})
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -14,10 +17,20 @@ exports.projectInfo = function(req, res) { 
 
 exports.addProject = function(req, res) {
   var form_data = req.body;
-  console.log(form_data);
 
-  // make a new Project and save it to the DB
-  // YOU MUST send an OK response w/ res.send();
+  var newProject = new models.Project( {
+    "title": form_data.project_title,
+    "date": form_data.date,
+    "summary": form_data.summary,
+    "image": form_data.image_url
+  });
+  
+  newProject.save(afterSaving);
+  function afterSaving(err) {
+    if(err) { console.log(err); res.send(500); }
+    console.log(newProject);
+    res.send();
+  }
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +38,14 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(afterQuery);
+
+    function afterQuery(err, projects) {
+      if(err) console.log(err);
+      res.send();
+    }
 }
